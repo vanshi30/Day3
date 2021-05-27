d9ac548f8e0fd01fbb5b02a2221e7aac829ce845
vanshi30 <73190551+vanshi30@users.noreply.github.com>
2021-05-27 04:14:54 -0700

    internship registration form using php
    use of html,css,php

---
+++ Internship_registration_form/process.php
@@ -0,0 +1,35 @@
+<html>
+    <head>
+            <title> congratulations</title>
+            <link rel="stylesheet" type="text/css" href="css/style2.css">
+    </head>
+    <body>
+
+    <?php
+    $a=$_POST['txt'];
+        echo"<table align='right'>";
+        echo "<tr>";
+        echo "<td> <br><br></td>";
+        echo "</tr>";
+        echo "<tr>";
+        echo "<td><br><br></td>";
+        echo "</tr>";
+        echo "<tr>";
+        echo "<td><br><br></td>";
+        echo "</tr>";
+        echo "<tr>";
+        echo "<td><br><br></td>";
+        echo "</tr>";
+        echo "<tr>";
+        echo "<td><br><br></td>";
+        echo "</tr>";
+        echo "<tr>";
+        echo "<td><br><br></td>";
+        echo "</tr>";
+        echo "<tr>";
+        echo"<td><h1><center><span id='c'>CONGRATULATIONS!</span>$a your registration is done</center></h1></td>";
+        echo"</tr>";
+        echo"</table>";
+    ?>
+    </body>
+</html>
